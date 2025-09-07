55faefa43e2df92a3f4b9f6a32377bce6d865b16
Cristian Molina <cm1094871@gmail.com>
2025-09-07 15:20:12 -0500

    funcion principal

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/postgres/fitzon/connect"
+)
+
 func main(){
-	
+	conexion, err := conexiondb.ConexionDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// cierre de conexion
+	defer conexion.Close()
+	fmt.Println("Conexion exitosa a PosgreSQL")
 }

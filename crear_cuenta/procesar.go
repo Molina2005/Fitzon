75a98b7a601b96d3566e88d27af9c6f7474bc314
Cristian Molina <cm1094871@gmail.com>
2025-09-09 19:40:19 -0500

    funcion procesar crear cuenta

---
+++ crear_cuenta/procesar.go
@@ -0,0 +1,12 @@
+package crearcuenta
+
+import (
+	"net/http"
+)
+
+// http.ResponserWriter : sirve para devolver la respuesta devuelta al cliente
+// http.Request lo que hace es recibir la peticion del usuario, toda la informacion que el cliente envia al servidor
+func ProcesarCrearCuenta(rw http.ResponseWriter, r *http.Request) {
+
+
+}
